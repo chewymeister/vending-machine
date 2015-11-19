394e9e56503a8e48b8b53f7618353a3e6f5a2924
Jonathan Tsang <chewymeister88@googlemail.com>
2015-11-19 02:09:30 +0000

    Change calculator can reduce the number of coins to represent monetary value by denominator

--- change_calculator_spec.rb
+++ change_calculator_spec.rb
@@ -6,10 +6,29 @@ require 'rspec'
 class ChangeCalculator
   def initialize amount
     @amount = amount
+    @coin_map = {
+      two_pound: 2.00,
+      one_pound: 1.00,
+      fifty_pee: 0.5,
+      twenty_pee: 0.2,
+      ten_pee: 0.1,
+      five_pee: 0.05,
+      one_pee: 0.01
+    }
   end
 
   def change amount
-    { two_pound: 1 }
+    puts "Initial amount: #{amount}"
+    required_coins = Hash.new(0)
+
+    @coin_map.each do |coin, value|
+      while amount >= value
+        required_coins[coin] += 1
+        amount -= value
+      end
+    end
+
+    required_coins
   end
 end
 
@@ -18,8 +37,15 @@ describe ChangeCalculator do
     it "should require 1 two pound coin" do
       amount = 2.00
       calculator = ChangeCalculator.new(amount)
-      required_coins = calculator.change 2.00
+      required_coins = calculator.change amount
       expect(required_coins).to eq({ two_pound: 1 })
+    end
+
+    it "should require 2 two pound coins" do
+      amount = 4.00
+      calculator = ChangeCalculator.new(amount)
+      required_coins = calculator.change amount
+      expect(required_coins).to eq({ two_pound: 2 })
     end
   end
 end
