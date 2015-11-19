d17edce4b9652fb2275e23b69390a1e14f140d9f
Jonathan Tsang <chewymeister88@googlemail.com>
2015-11-19 13:16:06 +0000

    Created examples to test in_stock?(coin) method with all coins, restructured test to only test coins in stock and not in stock

--- till_spec.rb
+++ till_spec.rb
@@ -1,7 +1,3 @@
-# make sure till keeps record of coins in stock
-# test dispense! method
-# test in_stock? method
-
 require 'rspec'
 
 class Till
@@ -17,7 +13,7 @@ class Till
   }
 
   def initialize
-    @stock = FULL_TILL.clone
+    @stock = FULL_TILL.dup
   end
 
   def in_stock? coin
@@ -30,23 +26,64 @@ class Till
 end
 
 describe Till do
-  context "#in_stock?" do
-    context "when coins are in stock" do
-      it "should return true for two pound coin" do
-        till = Till.new
-        expect(till.in_stock?(:two_pound)).to be true
-      end
+  context "when coins are in stock" do
+    it "should return true for two pound coin" do
+      till = Till.new
 
+      expect(till.in_stock?(:two_pound)).to be true
     end
 
-    context "when coins are not in stock" do
-      it "should return false for two pound coin" do
-        till = Till.new
-        10.times do |n|
-          till.dispense!(:two_pound)
-        end
-        expect(till.in_stock?(:two_pound)).to be false
+    it "should return true for one pound coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:one_pound)).to be true
+    end
+
+    it "should return true for fifty pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:fifty_pee)).to be true
+    end
+
+    it "should return true for twenty pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:twenty_pee)).to be true
+    end
+
+    it "should return true for ten pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:ten_pee)).to be true
+    end
+
+    it "should return true for five pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:five_pee)).to be true
+    end
+
+    it "should return true for two pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:two_pee)).to be true
+    end
+
+    it "should return true for one pee coin" do
+      till = Till.new
+
+      expect(till.in_stock?(:one_pee)).to be true
+    end
+  end
+
+  context "when coins are not in stock" do
+    it "should return false for two pound coin" do
+      till = Till.new
+      10.times do |n|
+        till.dispense!(:two_pound)
       end
+
+      expect(till.in_stock?(:two_pound)).to be false
     end
   end
 end
